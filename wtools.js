731b7199df2cb328864e2fad6d57f40cff4c7464
Jose F Rivas C <josefelixrc7@gmail.com>
2024-07-20 12:31:58 -0400

    feat: Add RowTable class

--- wtools.js
+++ wtools.js
@@ -362,5 +362,27 @@ var wtools =
             });
             return response;
         }
-    }    
+    }
+    ,RowTable: class
+    {
+        constructor(data)
+        {
+            this.data = data;
+            this.customs = []
+        }
+        Build_ = (table, custom_rows_lambda) =>
+        {
+            for(let row of this.data)
+            {
+                let tr = $('<tr></tr>');
+    
+                let fields = custom_rows_lambda(row);
+    
+                for(let field of fields)
+                    $(tr).append($(field));
+    
+                $(table).append(tr);
+            }
+        }
+    }
 };
